186fc285eb845a2d27d4b115d9015456d1cd5ccb
Vsevolod Shchur <sevaxxx.sx@gmail.com>
2018-12-14 17:03:57 +0500

    Еще тесты

---
+++ tests/AlcoholNodeTest.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AlcoholNodeTest {
+    @Test
+    public void fillCommands() {
+        var node = new AlcoholNode();
+        var nextNode = new StartNode();
+        var map = QuizNode.alcoholOptions;
+        node.fillCommands(nextNode, map);
+        Assert.assertEquals(map.size(), node.commands.size());
+        for (var a : node.commands.keySet()){
+            Assert.assertEquals(nextNode, node.commands.get(a).nextNode);
+        }
+    }
+}
---
+++ tests/BirthdayNodeTest.java
@@ -0,0 +1,31 @@
+import org.junit.*;
+
+public class BirthdayNodeTest {
+
+    @Test
+    public void fillCommands() {
+        var node = new BirthdayNode();
+        var nextNode = new StartNode();
+        node.fillCommands(nextNode);
+        Assert.assertEquals(1, node.commands.size());
+        Assert.assertEquals(nextNode, node.commands.get("").nextNode);
+    }
+
+    @Test
+    public void viewHelp() {
+        var node = new BirthdayNode();
+        var nextNode = new StartNode();
+        node.fillCommands(nextNode);
+        var response = node.viewHelp();
+        Assert.assertEquals("Type your birthday (dd.mm.yyyy)", response.userRespond);
+        Assert.assertSame(response.userRespond, node.getAnswer("help").sup.get().userRespond);
+    }
+
+    @Test
+    public void getCommandList() {
+        var node = new BirthdayNode();
+        var nextNode = new StartNode();
+        node.fillCommands(nextNode);
+        Assert.assertEquals(0,node.getCommandList().size());
+    }
+}
